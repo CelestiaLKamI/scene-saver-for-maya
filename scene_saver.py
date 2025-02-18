b0fd1b87754f826aa14e994a0a49c0b1e00d1e67
CelestiaLKamI <work.mayukh14@gmail.com>
2025-02-18 22:17:56 +0530

    Add scene_saver.py

---
+++ scene_saver.py
@@ -0,0 +1,5 @@
+import maya.cmds as cmds
+import maya.utils as utils
+import os
+from PySide2.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QPushButton, QRadioButton, QCheckBox, QComboBox, QDoubleSpinBox, QLineEdit, QTreeWidget, QTreeWidgetItem
+from PySide2.QtCore import Qt
